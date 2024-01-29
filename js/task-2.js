d8a65b9be1bdbafa71c6c8782579b116e4d266aa
AleksandraCherevko <119119431+AleksandraCherevko@users.noreply.github.com>
2024-01-29 20:32:45 +0100

    update task 2

--- js/task-2.js
+++ js/task-2.js
@@ -1,8 +1,8 @@
 function formatMessage(message, maxLength) {
-  if (message <= maxLength) {
+  if (message.length <= maxLength) {
     return message;
   } else {
-    return `${message.slice(0, maxLength)}${`...`}`;
+    return message.slice(0, maxLength) + "...";
   }
 }
 
